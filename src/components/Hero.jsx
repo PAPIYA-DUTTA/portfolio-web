361b8d23d9bfa67e95336b465c3f2aaad1b4d24b
PAPIYA-DUTTA <papiyad258@gmail.com>
2024-06-06 23:30:01 +0530

    added-app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
        </h1>
        
        <div className='my-8'>
-         <a href='Highlighted expertise in integrating backend services, enhancing functionality and improving user satisfaction by 40%' className='px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white '>Resume</a>
+         <a href='https://drive.google.com/file/d/104TdTkubzvYUu7AE-gDW6gly6ZbDoNxO/view?usp=drivesdk' className='px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-br from-orange-500 to-pink-500 text-white '>Resume</a>
        
         
          <a href='#contact' className='px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-br from-orange-500 to-pink-500 text-white hover:border-none'>Contact</a>
